f76eedc728eb2e68251ccd1f9764b7ab9fe1089e
josephk <86959956+josephk-O@users.noreply.github.com>
2025-04-14 22:03:57 +0000

    feat: update assessment data structure and improve assessment history page
    - Refactored Assessment interface to change assessmentData to assessment_data for consistency.
    - Updated HistoryPage to use the new assessment_data structure and removed unnecessary state declarations.
    - Modified ResultsPage to align with the new assessment data structure, simplifying the assessmentData object.

--- backend/services/supabaseService.js
+++ backend/services/supabaseService.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 // Create a Supabase client
 const supabase = createClient(
-  'https://nooizeyjujtddtxkirof.supabase.co', // Supabase URL (derived from the anon key)
+  process.env.SUPABASE_URL, // Supabase URL (derived from the anon key)
   process.env.SUPABASE_ANON_PUBLIC
 );
 
